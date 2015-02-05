579b4da73446836ba11b1cde776942d4a1f80453
hex7c0 <carniellifrancesco@gmail.com>
2015-02-05 09:51:08 +0100

    tab 2 size

--- index.js
+++ index.js
@@ -22,133 +22,133 @@
  */
 function wrapper(my) {
 
-    /**
-     * response
-     * 
-     * @function wrapper
-     * @param {Object} req - client request socket
-     * @param {Array} start - start time
-     * @param {Boolean} clean - clean var
-     * @return {String}
-     */
-    function response(req, start, clean) {
+  /**
+   * response
+   * 
+   * @function wrapper
+   * @param {Object} req - client request socket
+   * @param {Array} start - start time
+   * @param {Boolean} clean - clean var
+   * @return {String}
+   */
+  function response(req, start, clean) {
 
-        var diff = process.hrtime(start);
-        diff = diff[0] * 1e9 + diff[1]; // nanosecond
-        var data = (req._bytesDispatched * my.byte) / my.misura;
-        if (clean === true) {
-            first = new Array(2);
-            story._bytesDispatched = 0;
-        }
-        return (data / (diff / my.time)).toFixed(1) + my.out;
+    var diff = process.hrtime(start);
+    diff = diff[0] * 1e9 + diff[1]; // nanosecond
+    var data = (req._bytesDispatched * my.byte) / my.misura;
+    if (clean === true) {
+      first = new Array(2);
+      story._bytesDispatched = 0;
+    }
+    return (data / (diff / my.time)).toFixed(1) + my.out;
+  }
+
+  /**
+   * request
+   * 
+   * @function wrapper
+   * @param {Object} req - client request socket
+   * @param {Array} start - start time
+   * @param {Boolean} clean - clean var
+   * @return {String}
+   */
+  function request(req, start, clean) {
+
+    var diff = process.hrtime(start);
+    diff = diff[0] * 1e9 + diff[1]; // nanosecond
+    var data = (req.bytesRead * my.byte) / my.misura;
+    if (clean === true) {
+      first = new Array(2);
+      story.bytesRead = 0;
+    }
+    return (data / (diff / my.time)).toFixed(1) + my.out;
+  }
+
+  var oi = response;
+  if (!my.response) {
+    oi = request;
+  }
+  var first = new Array(2);
+  var story = {
+    _bytesDispatched: 0,
+    bytesRead: 0
+  };
+
+  /**
+   * rate
+   * 
+   * @function rate
+   * @param {Object} req - client request socket
+   * @param {Array} start - start time
+   * @return {String}
+   */
+  return function rate(req, start) {
+
+    if (req === null || typeof (req) !== 'object') {
+      throw new TypeError('req required');
+    }
+    if (start === null || Array.isArray(start) === false) {
+      throw new TypeError('start required');
+    }
+    var res = req.res;
+    if (first[1] === undefined) {
+      first = start;
+    }
+
+    // single
+    var t = res.finished || (res.socket && res.socket.writable === false);
+    if (t === true) {
+      req.transferRate = oi(req.socket, first);
+      return req.transferRate;
+    }
+
+    // chunk
+    var soc = req.socket;
+    /**
+     * close callback
+     * 
+     * @function close
+     */
+    function close() {
+
+      story._bytesDispatched += soc._bytesDispatched;
+      story.bytesRead += soc.bytesRead;
+      return;
     }
 
     /**
-     * request
+     * finish callback
      * 
-     * @function wrapper
-     * @param {Object} req - client request socket
-     * @param {Array} start - start time
-     * @param {Boolean} clean - clean var
-     * @return {String}
+     * @function finish
      */
-    function request(req, start, clean) {
+    function finish() {
 
-        var diff = process.hrtime(start);
-        diff = diff[0] * 1e9 + diff[1]; // nanosecond
-        var data = (req.bytesRead * my.byte) / my.misura;
-        if (clean === true) {
-            first = new Array(2);
-            story.bytesRead = 0;
-        }
-        return (data / (diff / my.time)).toFixed(1) + my.out;
+      cleanup();
+      if (first[1] !== undefined) {
+        story._bytesDispatched += soc._bytesDispatched;
+        story.bytesRead += soc.bytesRead;
+        req.transferRate = oi(story, first, true);
+        return req.transferRate;
+      }
+      return;
     }
-
-    var oi = response;
-    if (!my.response) {
-        oi = request;
-    }
-    var first = new Array(2);
-    var story = {
-        _bytesDispatched: 0,
-        bytesRead: 0
-    };
 
     /**
-     * rate
+     * clear listener
      * 
-     * @function rate
-     * @param {Object} req - client request socket
-     * @param {Array} start - start time
-     * @return {String}
+     * @function cleanup
      */
-    return function rate(req, start) {
+    function cleanup() {
 
-        if (req === null || typeof (req) !== 'object') {
-            throw new TypeError('req required');
-        }
-        if (start === null || Array.isArray(start) === false) {
-            throw new TypeError('start required');
-        }
-        var res = req.res;
-        if (first[1] === undefined) {
-            first = start;
-        }
+      // soc.removeListener('finish', finish);
+      soc.removeListener('close', close);
+      return;
+    }
 
-        // single
-        var t = res.finished || (res.socket && res.socket.writable === false);
-        if (t === true) {
-            req.transferRate = oi(req.socket, first);
-            return req.transferRate;
-        }
-
-        // chunk
-        var soc = req.socket;
-        /**
-         * close callback
-         * 
-         * @function close
-         */
-        function close() {
-
-            story._bytesDispatched += soc._bytesDispatched;
-            story.bytesRead += soc.bytesRead;
-            return;
-        }
-
-        /**
-         * finish callback
-         * 
-         * @function finish
-         */
-        function finish() {
-
-            cleanup();
-            if (first[1] !== undefined) {
-                story._bytesDispatched += soc._bytesDispatched;
-                story.bytesRead += soc.bytesRead;
-                req.transferRate = oi(story, first, true);
-                return req.transferRate;
-            }
-            return;
-        }
-
-        /**
-         * clear listener
-         * 
-         * @function cleanup
-         */
-        function cleanup() {
-
-            // soc.removeListener('finish', finish);
-            soc.removeListener('close', close);
-            return;
-        }
-
-        soc.once('close', finish);
-        // soc.once('finish', finish);
-        return;
-    };
+    soc.once('close', finish);
+    // soc.once('finish', finish);
+    return;
+  };
 }
 
 /**
@@ -161,69 +161,69 @@ function wrapper(my) {
  */
 function transfer(opt) {
 
-    var options = opt || Object.create(null);
-    var my = {
-        misura: 1024,
-        byte: 1,
-        out: ' KB/s',
-        time: 1000000000,
-        response: true
-    };
+  var options = opt || Object.create(null);
+  var my = {
+    misura: 1024,
+    byte: 1,
+    out: ' KB/s',
+    time: 1000000000,
+    response: true
+  };
 
-    // if (Boolean(options.Bps)) {
-    if (options.data == 'Byte') {
-        my.misura = 1;
-        my.byte = 1;
-        my.out = ' Byte';
-    } else if (options.data == 'KB') {
-        // } else if (Boolean(options.KBps)) {
-        my.misura = 1024;
-        my.byte = 1;
-        my.out = ' KB';
-    } else if (options.data == 'MB') {
-        // } else if (Boolean(options.MBps)) {
-        my.misura = 1048576;
-        my.byte = 1;
-        my.out = ' MB';
-    } else if (options.data == 'bit') {
-        // } else if (Boolean(options.bps)) {
-        my.misura = 1;
-        my.byte = 8;
-        my.out = ' bit';
-    } else if (options.data == 'Kb') {
-        // } else if (Boolean(options.kbps)) {
-        my.misura = 1000;
-        my.byte = 8;
-        my.out = ' Kbit';
-    } else if (options.data == 'Mb') {
-        // } else if (Boolean(options.Mbps)) {
-        my.misura = 1000000;
-        my.byte = 8;
-        my.out = ' Mbit';
-    }
+  // if (Boolean(options.Bps)) {
+  if (options.data == 'Byte') {
+    my.misura = 1;
+    my.byte = 1;
+    my.out = ' Byte';
+  } else if (options.data == 'KB') {
+    // } else if (Boolean(options.KBps)) {
+    my.misura = 1024;
+    my.byte = 1;
+    my.out = ' KB';
+  } else if (options.data == 'MB') {
+    // } else if (Boolean(options.MBps)) {
+    my.misura = 1048576;
+    my.byte = 1;
+    my.out = ' MB';
+  } else if (options.data == 'bit') {
+    // } else if (Boolean(options.bps)) {
+    my.misura = 1;
+    my.byte = 8;
+    my.out = ' bit';
+  } else if (options.data == 'Kb') {
+    // } else if (Boolean(options.kbps)) {
+    my.misura = 1000;
+    my.byte = 8;
+    my.out = ' Kbit';
+  } else if (options.data == 'Mb') {
+    // } else if (Boolean(options.Mbps)) {
+    my.misura = 1000000;
+    my.byte = 8;
+    my.out = ' Mbit';
+  }
 
-    if (options.time == 'nanosecond') {
-        // if (Boolean(options.nanosecond)) {
-        my.time = 1;
-        my.out += '/ns';
-    } else if (options.time == 'millisecond') {
-        // } else if (Boolean(options.millisecond)) {
-        my.time = 1000000;
-        my.out += '/ms';
-    } else if (options.time == 'second') {
-        // } else if (Boolean(options.second)) {
-        my.time = 1000000000;
-        my.out += '/s';
-    }
+  if (options.time == 'nanosecond') {
+    // if (Boolean(options.nanosecond)) {
+    my.time = 1;
+    my.out += '/ns';
+  } else if (options.time == 'millisecond') {
+    // } else if (Boolean(options.millisecond)) {
+    my.time = 1000000;
+    my.out += '/ms';
+  } else if (options.time == 'second') {
+    // } else if (Boolean(options.second)) {
+    my.time = 1000000000;
+    my.out += '/s';
+  }
 
-    if (options.response === false) {
-        my.response = false;
-    }
+  if (options.response === false) {
+    my.response = false;
+  }
 
-    if (options.output === false) {
-        my.out = '';
-    }
+  if (options.output === false) {
+    my.out = '';
+  }
 
-    return wrapper(my);
+  return wrapper(my);
 }
 module.exports = transfer;
